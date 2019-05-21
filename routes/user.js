78590a27c34d5f7df582a31c33bf36ad4ceb103a
Charlie <chen_610146@hotmail.com>
2019-05-21 20:55:57 +0800

    Add register function

--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,24 @@ const router = express.Router()
 const db = require('../models')
 const User = db.User
 const bcrypt = require('bcryptjs')
+const passport = require('passport')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-  res.send('Login send')
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) { console.log(err) }
+    if (!user) {
+      req.flash('warning_msg', info.message)
+      return res.redirect('/users/login')
+    }
+    req.logIn(user, (err) => {
+      if (err) { console.log(err) }
+      return res.redirect('/')
+    })
+  })(req, res, next)
 })
 
 router.get('/register', (req, res) => {
@@ -17,7 +28,39 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  res.send('Register send')
+  const { name, email, password, confirmPassword } = req.body
+
+  const errors = []
+
+  if (password !== confirmPassword) {
+    errors.push({ message: 'Password confirm failure' })
+  }
+
+  User.findOne({ where: { email: email } }).then(user => {
+    if (user) {
+      errors.push({ message: 'The email already be registered' })
+      res.render('register', { name, email, password, confirmPassword, errors })
+    } else {
+      const newUser = new User({
+        name,
+        email,
+        password,
+      })
+      bcrypt.genSalt(10, (err, salt) => [
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err
+          newUser.password = hash
+
+          newUser.save().then(user => {
+            return res.redirect('/users/login')
+          }).catch(err => {
+            console.log(err)
+          })
+        })
+      ])
+
+    }
+  })
 })
 
 module.exports = router
