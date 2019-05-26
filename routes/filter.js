f250405307df8f48adf226ad7b5e8b0ce9e59f63
Charlie <chen_610146@hotmail.com>
2019-05-26 13:40:28 +0800

    filter function edit

--- routes/filter.js
+++ routes/filter.js
@@ -4,38 +4,22 @@ const db = require('../models')
 const Record = db.record
 
 router.get('/', (req, res, next) => {
-  let month = req.query.month || false
-  let category = req.query.category
+  const month = req.query.month
+  const category = req.query.category
+  const queryObject = { where: { userId: req.user.id } }
 
-  if (category === '') {
-    Record.findAll({ where: { userId: req.user.id } }).then(records => {
-
-      if (month) {
-        records = records.filter(record => {
-          let date = new Date(record.date)
-          if (month) {
-            return date.getMonth() === month - 1
-          }
-          return true
-        })
-        res.render('index', { records, month, category })
-      }
-    })
-  } else {
-    Record.findAll({ where: { category: category, userId: req.user.id } }).then(records => {
-
-      if (month) {
-        records = records.filter(record => {
-          let date = new Date(record.date)
-          if (month) {
-            return date.getMonth() === month - 1
-          }
-          return true
-        })
-        res.render('index', { records, month, category })
-      }
-    })
+  if (category) {
+    queryObject.where.category = category
   }
+
+  Record.findAll(queryObject).then(records => {
+
+    records = records.filter(record => {
+      return !month || new Date(record.date).getMonth() === month - 1
+    })
+    res.render('index', { records, month, category })
+
+  })
 
 })
 
